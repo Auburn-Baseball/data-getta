52f6691ea96dbed79d4870f4f7261ffe770ff094
Jacob Murrah <110310485+jmurrah@users.noreply.github.com>
2025-09-19 16:38:12 -0500

    User Sign Up/Login Functionality (#10)

---
+++ ui/src/utils/supabase/auth.ts
@@ -0,0 +1,24 @@
+import { supabase } from '@/utils/supabase/client';
+
+export async function login(email: string, password: string) {
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+  if (error) throw error;
+  return data;
+}
+
+export async function signup(email: string, password: string) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+  if (error) throw error;
+  return data;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    throw error;
+  }
+}
